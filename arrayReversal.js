03d5f5a4244f3d46fba7d414d14315cf6157e36a
orneto-incm <orneto11@gmail.com>
2025-08-23 23:21:56 +0530

    reversing array

--- arrayReversal.js
+++ arrayReversal.js
@@ -4,11 +4,15 @@
  output array = [5,0,4,2,1]
 **/
 
-// Method 2
+// Method 1 - Bruteforce Method
 // arr1 = [1, 2, 5, 6, 7];
-arr1 = [2];
+arr = [1, 2, 5, 6, 7];
 function reverseArray(inputArray){
-    
-
+    let reversedArray = [];
+    let n = inputArray.length -1;
+        for(let j=n; j>=0; j--){
+            reverseArray.push(arr[j]);
+        }
+    return reversedArray;
 }
-reverseArray(arr1);
+console.log(reverseArray(arr));
