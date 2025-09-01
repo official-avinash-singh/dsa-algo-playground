c12ff1438872dbf6e2282b1b3eafef4d9ca77017
official-avinash-singh <business.singhavinash@gmail.com>
2025-09-02 01:20:50 +0530

    second largest num in array

---
+++ array/secondLargestNumInArray.js
@@ -0,0 +1,17 @@
+/*
+arr = [1,3,5,6,7]
+Find the second largest number in the array
+*/
+let arr = [1,3,6,5,7,9,10,4];
+let secondLargest = (arr) =>{
+    let largest = arr[0];
+    let secondLargestnum;
+    for(let i = 1; i<arr.length; i++){
+        if(arr[i]>arr[i-1]){
+            secondLargestnum = largest;
+            largest = arr[i];
+        }
+    }
+    return secondLargestnum;
+}
+console.log(secondLargest(arr));
